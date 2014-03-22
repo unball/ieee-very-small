bdc2a9565ac339ecfd5f0f16f2a094059bdba22a
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2014-03-22 13:44:02 -0300

    Added error checking for arguments and image loading. Checked code style. Wrote some error and info messages.

--- software/src/dummy_camera/dummy_camera.cpp
+++ software/src/dummy_camera/dummy_camera.cpp
@@ -1,11 +1,13 @@
 /**
  * @file   dummy_camera.cpp
  * @author Gabriel Naves da Silva
+ * @author Matheus Vieira Portela
  * @date   21/03/2014
  *
  * @attention Copyright (C) 2014 UnBall Robot Soccer Team
  *
  * @brief  Dummy camera node
+ * 
  * Loads an image file and publishes it on the "camera/image_raw" topic.
  */
 
@@ -14,23 +16,43 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "dummy_camera");
     ros::NodeHandle nh;
+    ros::Rate loop_rate(1); // Publishes the image at a rate of 1 message per second.
     image_transport::ImageTransport it(nh); // Used to publish and subscribe to images.
     image_transport::Publisher pub = it.advertise("camera/image_raw", 1);
+    cv::Mat image;
     cv_bridge::CvImage frame;
 
-    frame.encoding = sensor_msgs::image_encodings::BGR8; // Sets the encoding of the frame.
-    frame.image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR); // Loads the image given as an argument.
+    // Check if enough arguments where given
+    if( argc != 2)
+    {
+        ROS_ERROR("Not enough arguments. Usage: dummy_camera <image file>");
+        return -1;
+    }
 
-    // Publishes the image at a rate of 1 message per second.
-    ros::Rate loop_rate(1);
-    while (nh.ok()) {
+    // Load image and check for errors
+    image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    
+    if (!image.data)
+    {
+        ROS_ERROR("Could not open or find the image");
+        return -1;
+    }
+    
+    // Set frame encoding and data
+    frame.encoding = sensor_msgs::image_encodings::BGR8;
+    frame.image = image;
+
+    // Publish image
+    while (ros::ok())
+    {
+        ROS_INFO("Sending image");
         pub.publish(frame.toImageMsg());
+        ros::spinOnce();
         loop_rate.sleep();
     }
 
