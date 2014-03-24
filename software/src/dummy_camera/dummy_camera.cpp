c9147f0304da9e5c2ce682833af951c2ec2ca90e
Gabriel Naves <gabri.navess@gmail.com>
2014-03-24 14:36:32 -0300

    Modified the dummy_camera node, wich now publishes a video to the /camera/image_raw topic.

--- software/src/dummy_camera/dummy_camera.cpp
+++ software/src/dummy_camera/dummy_camera.cpp
@@ -8,7 +8,7 @@
  *
  * @brief  Dummy camera node
  * 
- * Loads an image file and publishes it on the "camera/image_raw" topic.
+ * Loads a video file and publishes it on the "camera/image_raw" topic.
  */
 
 #include <ros/ros.h>
@@ -21,39 +21,45 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "dummy_camera");
     ros::NodeHandle nh;
-    ros::Rate loop_rate(1); // Publishes the image at a rate of 1 message per second.
     image_transport::ImageTransport it(nh); // Used to publish and subscribe to images.
     image_transport::Publisher pub = it.advertise("camera/image_raw", 1);
-    cv::Mat image;
     cv_bridge::CvImage frame;
+    int frame_counter = 0; // Used to count the number of frames published on the topic.
+    int frame_number;
 
     // Check if enough arguments where given
     if( argc != 2)
     {
-        ROS_ERROR("Not enough arguments. Usage: dummy_camera <image file>");
+        ROS_ERROR("Not enough arguments. Usage: dummy_camera <video file>");
         return -1;
     }
 
-    // Load image and check for errors
-    image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    
-    if (!image.data)
+    // Load video and check for errors
+    cv::VideoCapture cap(argv[1]);
+    if (!cap.isOpened())
     {
-        ROS_ERROR("Could not open or find the image");
+        ROS_ERROR("Could not open of find the video file");
         return -1;
     }
-    
-    // Set frame encoding and data
+
+    // Set the loop rate, defined by the framerate of the video
+    ros::Rate loop_rate(cap.get(CV_CAP_PROP_FPS));
+
+    // Set frame encoding
     frame.encoding = sensor_msgs::image_encodings::BGR8;
-    frame.image = image;
 
-    // Publish image
-    while (ros::ok())
+    // Retrieve amount of frames on the video
+    frame_number = cap.get(CV_CAP_PROP_FRAME_COUNT);
+
+    // Publish the video
+    ROS_INFO("Sending video");
+    while (ros::ok() && frame_counter < frame_number)
     {
-        ROS_INFO("Sending image");
+        cap >> frame.image; // Get a new frame from the video capture
         pub.publish(frame.toImageMsg());
         ros::spinOnce();
         loop_rate.sleep();
+        frame_counter++; // Counts until the number of frames on the video
     }
 
     return 0;
