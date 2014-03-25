b8c3b6f44f4d6ef6b87ac5fef6ed0125c748ee3d
Gabriel Naves <gabri.navess@gmail.com>
2014-03-25 12:56:19 -0300

    Added test_dummy_camera node. It subscribes to the /camera/image_raw topic and displays image messages published to it on the screen

---
+++ software/src/dummy_camera/test/test_dummy_camera.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file   test_dummy_camera
+ * @author Gabriel Naves da Silva
+ * @date   25/03/2014
+ *
+ * @attention Copyright (C) 2014 UnBall Robot Soccer Team
+ *
+ * @brief  Dummy camera testing node
+ *
+ * Subscribes to the /camera/image_raw topic and shows each
+ * image published by the dummy_camera node.
+ */
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/highgui/highgui.hpp>
+
+static const std::string OPENCV_WINDOW = "Image window";
+
+/**
+ * Callback function for the image subscriber.
+ * @param msg the message received.
+ */
+void imageCb(const sensor_msgs::ImageConstPtr& msg)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    // Copies the image data to cv_ptr and handles exceptions
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+    
+    //Update GUI Window
+    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    cv::waitKey(1);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "test_dummy_camera");
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber sub;
+    
+    sub = it.subscribe("/camera/image_raw", 1, imageCb); // Subscribes to /camera/image_raw topic
+    
+    cv::namedWindow(OPENCV_WINDOW);
+    ros::spin();
+    cv::destroyWindow(OPENCV_WINDOW);
+
+    return 0;
+}
+
