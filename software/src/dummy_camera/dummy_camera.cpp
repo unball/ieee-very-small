a164a22e29578e0327b9f022e861db2d3cdca96b
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2014-03-27 11:24:57 -0300

    Improving dummy_camera and test with debug messages and error checking.

--- software/src/dummy_camera/dummy_camera.cpp
+++ software/src/dummy_camera/dummy_camera.cpp
@@ -17,15 +17,15 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/highgui/highgui.hpp>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "dummy_camera");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh); // Used to publish and subscribe to images.
     image_transport::Publisher pub = it.advertise("camera/image_raw", 1);
     cv_bridge::CvImage frame;
-    int frame_counter = 0; // Used to count the number of frames published on the topic.
-    int frame_number;
+    int frame_counter; // Used to count the number of frames published on the topic.
+    int num_frames;
 
     // Check if enough arguments where given
     if( argc != 2)
@@ -44,23 +44,26 @@ int main(int argc, char** argv)
 
     // Set the loop rate, defined by the framerate of the video
     ros::Rate loop_rate(cap.get(CV_CAP_PROP_FPS));
+    ROS_DEBUG("Loop rate: %lf", cap.get(CV_CAP_PROP_FPS));
 
     // Set frame encoding
     frame.encoding = sensor_msgs::image_encodings::BGR8;
 
     // Retrieve amount of frames on the video
-    frame_number = cap.get(CV_CAP_PROP_FRAME_COUNT);
+    num_frames = cap.get(CV_CAP_PROP_FRAME_COUNT);
+    ROS_DEBUG("Frame number: %d", num_frames);
 
     // Publish the video
     ROS_INFO("Sending video");
-    while (ros::ok() && frame_counter < frame_number)
+    for (frame_counter = 0; ros::ok() && (frame_counter < num_frames); frame_counter++)
     {
+        ROS_DEBUG("Frame counter: %d", frame_counter);
         cap >> frame.image; // Get a new frame from the video capture
         pub.publish(frame.toImageMsg());
         ros::spinOnce();
         loop_rate.sleep();
-        frame_counter++; // Counts until the number of frames on the video
     }
+    ROS_INFO("Finished sending video");
 
     return 0;
 }
--- software/src/dummy_camera/test/test_dummy_camera.cpp
+++ software/src/dummy_camera/test/test_dummy_camera.cpp
@@ -1,6 +1,7 @@
 /**
  * @file   test_dummy_camera
  * @author Gabriel Naves da Silva
+ * @author Matheus Vieira Portela
  * @date   25/03/2014
  *
  * @attention Copyright (C) 2014 UnBall Robot Soccer Team
@@ -18,31 +19,42 @@
 #include <opencv2/highgui/highgui.hpp>
 
 static const std::string OPENCV_WINDOW = "Image window";
+int frame_counter = 0;
 
 /**
  * Callback function for the image subscriber.
  * @param msg the message received.
  */
-void imageCb(const sensor_msgs::ImageConstPtr& msg)
+void imageCb(const sensor_msgs::ImageConstPtr &msg)
 {
     cv_bridge::CvImagePtr cv_ptr;
+    
     // Copies the image data to cv_ptr and handles exceptions
     try
     {
         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     }
-    catch (cv_bridge::Exception& e)
+    catch (cv_bridge::Exception &e)
     {
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
+    
+    if (!(cv_ptr->image.rows) || !(cv_ptr->image.cols))
+    {
+        ROS_ERROR("cv_ptr error: invalid image frame received");
+        exit(1);
+    }
+    
+    ROS_DEBUG("Frame counter: %d", frame_counter);
+    frame_counter++;
     
     //Update GUI Window
     cv::imshow(OPENCV_WINDOW, cv_ptr->image);
     cv::waitKey(1);
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "test_dummy_camera");
     ros::NodeHandle nh;
