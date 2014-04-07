eac32206289a15dd60ac657d34a6fa81b610b52d
Gabriel Naves <gabri.navess@gmail.com>
2014-04-07 11:03:50 -0300

    Deleted show_dummy_camera_node. Also made some changes to the dummy_camera node, which now publishes to the rgb image topic and the depth image topic.

--- software/src/dummy_camera/dummy_camera.cpp
+++ software/src/dummy_camera/dummy_camera.cpp
@@ -22,35 +22,45 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "dummy_camera");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh); // Used to publish and subscribe to images.
-    image_transport::Publisher pub = it.advertise("camera/image_raw", 1);
-    cv_bridge::CvImage frame;
+    image_transport::Publisher rgb_pub = it.advertise("/camera/rgb/image_raw", 1);
+    image_transport::Publisher depth_pub = it.advertise("/camera/depth/image_raw", 1);
+    cv_bridge::CvImage rgb_frame, depth_frame;
     int frame_counter; // Used to count the number of frames published on the topic.
     int num_frames;
 
     // Check if enough arguments where given
-    if( argc != 2)
+    if( argc != 3)
     {
-        ROS_ERROR("Not enough arguments. Usage: dummy_camera <video file>");
+        ROS_ERROR("Not enough arguments. Usage: dummy_camera <rgb video file> <depth video file>");
         return -1;
     }
 
-    // Load video and check for errors
-    cv::VideoCapture cap(argv[1]);
-    if (!cap.isOpened())
+    // Load rgb video and check for errors
+    cv::VideoCapture rgb_cap(argv[1]);
+    if (!rgb_cap.isOpened())
     {
-        ROS_ERROR("Could not open of find the video file");
+        ROS_ERROR("Could not open of find the rgb video file");
+        return -1;
+    }
+
+    // Load depth video and check for errors
+    cv::VideoCapture depth_cap(argv[2]);
+    if (!depth_cap.isOpened())
+    {
+        ROS_ERROR("Could not open of find the depth video file");
         return -1;
     }
 
     // Set the loop rate, defined by the framerate of the video
-    ros::Rate loop_rate(cap.get(CV_CAP_PROP_FPS));
-    ROS_DEBUG("Loop rate: %lf", cap.get(CV_CAP_PROP_FPS));
+    ros::Rate loop_rate(rgb_cap.get(CV_CAP_PROP_FPS));
+    ROS_DEBUG("Loop rate: %lf", rgb_cap.get(CV_CAP_PROP_FPS));
 
-    // Set frame encoding
-    frame.encoding = sensor_msgs::image_encodings::BGR8;
+    // Set rgb and depth frame encoding
+    rgb_frame.encoding = sensor_msgs::image_encodings::BGR8;
+    depth_frame.encoding = sensor_msgs::image_encodings::MONO8;
 
     // Retrieve amount of frames on the video
-    num_frames = cap.get(CV_CAP_PROP_FRAME_COUNT);
+    num_frames = rgb_cap.get(CV_CAP_PROP_FRAME_COUNT);
     ROS_DEBUG("Frame number: %d", num_frames);
 
     // Publish the video
@@ -58,8 +68,10 @@ int main(int argc, char **argv)
     for (frame_counter = 0; ros::ok() && (frame_counter < num_frames); frame_counter++)
     {
         ROS_DEBUG("Frame counter: %d", frame_counter);
-        cap >> frame.image; // Get a new frame from the video capture
-        pub.publish(frame.toImageMsg());
+        rgb_cap >> rgb_frame.image; // Get a new frame from the rgb video capture
+        depth_cap >> depth_frame.image; // Get a new frame from the depth video capture
+        rgb_pub.publish(rgb_frame.toImageMsg());
+        depth_pub.publish(depth_frame.toImageMsg());
         ros::spinOnce();
         loop_rate.sleep();
     }
