b67f00361cf4b3d0513793717b0647f0a49fe399
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2014-04-12 19:14:38 -0300

    Launching each robot node in a separate group, creating its own topic namespace. This allows them to be controlled individually. Updating simulator_node to control robots individually.

--- software/src/simulator/simulator_node.cpp
+++ software/src/simulator/simulator_node.cpp
@@ -13,7 +13,7 @@
 #include <geometry_msgs/Twist.h>
 #include <vector>
 
-void publishVel(ros::Publisher &publisher);
+void publishVel(ros::Publisher &publisher, float lin_vel, float rot_vel);
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "simulator_node");
@@ -21,11 +21,16 @@ int main(int argc, char **argv){
     ros::NodeHandle n;
     ros::Rate loop_rate(10);
     
-    ros::Publisher publisher = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    ros::Publisher publisher_1 = n.advertise<geometry_msgs::Twist>("robot_1/cmd_vel", 1);
+    ros::Publisher publisher_2 = n.advertise<geometry_msgs::Twist>("robot_2/cmd_vel", 1);
+    ros::Publisher publisher_3 = n.advertise<geometry_msgs::Twist>("robot_3/cmd_vel", 1);
+    ros::Publisher publisher_4 = n.advertise<geometry_msgs::Twist>("robot_4/cmd_vel", 1);
+    ros::Publisher publisher_5 = n.advertise<geometry_msgs::Twist>("robot_5/cmd_vel", 1);
+    ros::Publisher publisher_6 = n.advertise<geometry_msgs::Twist>("robot_6/cmd_vel", 1);
   
     while (ros::ok())
     {
-        publishVel(publisher);
+        publishVel(publisher_1, 0.5, 0.1);
         
         ros::spinOnce();
         loop_rate.sleep();
@@ -34,19 +39,19 @@ int main(int argc, char **argv){
     return(0);
 }
 
-void publishVel(ros::Publisher &publisher)
+void publishVel(ros::Publisher &publisher, float lin_vel, float rot_vel)
 {
     geometry_msgs::Twist msg;
     
     ROS_DEBUG("Publishing cmd vel");
     
-    msg.linear.x = 0.10; // Set to make the robot turn
+    msg.linear.x = rot_vel; // Set to make the robot turn
     msg.linear.y = 0.0;
     msg.linear.z = 0.0;
     
     msg.angular.x = 0.0;
     msg.angular.y = 0.0;
-    msg.angular.z = 0.5; // Set to make the robot move
+    msg.angular.z = lin_vel; // Set to make the robot move
     
     publisher.publish(msg);
 }
