5292a90639884275bce1589a976410e2d5256961
icaro <icaro@ulisses-i.(none)>
2014-05-13 12:50:32 -0300

    Created but didnt implement abstract class Play

---
+++ software/src/strategy/play.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file   play.cpp
+ * @author Icaro da Costa Mota
+ * @date   13/05/2014
+ *
+ * @attention Copyright (C) 2014 UnBall Robot Soccer Team
+ *
+ * @brief  play class
+ *
+ * Implements the methods that all plays must have
+ */
+
+#include "play.hpp"
+
---
+++ software/src/strategy/play.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file   play.hpp
+ * @author Icaro da Costa Mota
+ * @date   13/05/2014
+ *
+ * @attention Copyright (C) 2014 UnBall Robot Soccer Team
+ *
+ * @brief  Play class
+ *
+ * Defines the requisits of any given play. All plays will inherit from Play
+ */
+
+#ifndef UNBALL_ROBOT_H_
+#define UNBALL_ROBOT_H_
+
+class Play
+}
+  public:
+  private:
+};
+
+#endif  // UNBALL_ROBOT_H_
+
