1896c46882b976b79a6ba5c23f04ed428385cd0d
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2014-07-24 16:04:49 -0300

    Creating test suite for geometry/point.cpp

---
+++ software/src/test/geometry/test_point.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file   test_point.cpp
+ * @author Matheus Vieira Portela
+ * @date   24/07/2014
+ *
+ * @attention Copyright (C) 2014 UnBall Robot Soccer Team
+ *
+ * @brief Tests for point class
+ */
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <unball/geometry/point.hpp>
+
+TEST(TestPointClass, testDefaultConstructor)
+{
+    Point p;
+    EXPECT_EQ(p.getX(), 0);
+    EXPECT_EQ(p.getY(), 0);
+}
+
+TEST(TestPointClass, testConstructorWithParameters)
+{
+    Point p(10, 20);
+    EXPECT_EQ(p.getX(), 10);
+    EXPECT_EQ(p.getY(), 20);
+}
+
+TEST(TestPointClass, testSet1)
+{
+    Point p;
+    p.set(10, 20);
+    EXPECT_EQ(p.getX(), 10);
+    EXPECT_EQ(p.getY(), 20);
+}
+
+TEST(TestPointClass, testSet2)
+{
+    Point p;
+    p.set(-10, -20);
+    EXPECT_EQ(p.getX(), -10);
+    EXPECT_EQ(p.getY(), -20);
+}
+
+TEST(TestPointClass, testSet3)
+{
+    Point p;
+    p.set(Point(-10, -20));
+    EXPECT_EQ(p.getX(), -10);
+    EXPECT_EQ(p.getY(), -20);
+}
+
+TEST(TestPointClass, testMovePointWithAngle)
+{
+    Point p;
+    p.movePointWithAngle(10, 0);
+    EXPECT_EQ(p.getX(), 10);
+    EXPECT_EQ(p.getY(), 0);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
