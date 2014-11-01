118d1003bf262ccfb0a0992cbc0908ef6288bd71
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2014-11-01 09:26:53 -0200

    Forgot to in

---
+++ software/src/unball/include/unball/vision/tracked_object_type.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file   tracked_object.hpp
+ * @author Matheus Vieira Portela
+ * @date   07/08/2014
+ *
+ * @attention Copyright (C) 2014 UnBall Robot Soccer Team
+ *
+ * @brief Tracked object type
+ */
+
+#ifndef UNBALL_VISION_TRACKED_OBJECT_TYPE_H_
+#define UNBALL_VISION_TRACKED_OBJECT_TYPE_H_
+
+enum TrackedObjectType
+{
+    NONE,
+    FIELD,
+    ROBOT,
+    BALL
+};
+
+#endif // UNBALL_VISION_TRACKED_OBJECT_TYPE_H_
