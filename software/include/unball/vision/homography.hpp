bff0fc0d18826bf1978cb164dc631b593653f7ff
gabrielnaves <gabri.navess@gmail.com>
2015-02-27 16:39:57 -0300

    Created the new branch "issue39". Also, the initial homography class was created and it is now on CMakeLists.txt

---
+++ software/include/unball/vision/homography.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file   homography.hpp
+ * @author Gabriel Naves da Silva
+ * @date   27/02/2015
+ *
+ * @attention Copyright (C) 2014 UnBall Robot Soccer Team
+ *
+ * @brief Definition of the homography class
+ */
+
+#ifndef UNBALL_VISION_HOMOGRAPHY_H_
+#define UNBALL_VISION_HOMOGRAPHY_H_
+
+#include <opencv2/opencv.hpp>
+
+class Homography
+{
+  public:
+    Homography();
+    ~Homography();
+  private:
+};
+
+#endif // UNBALL_VISION_HOMOGRAPHY_H_
---
+++ software/src/unball/vision/homography.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file   homography.hpp
+ * @author Gabriel Naves da Silva
+ * @date   27/02/2015
+ *
+ * @attention Copyright (C) 2014 UnBall Robot Soccer Team
+ *
+ * @brief Implementation of the computer vision homography module
+ */
+
+#include <unball/vision/homography.hpp>
+
+Homography::Homography() {}
+
+Homography::~Homography() {}
