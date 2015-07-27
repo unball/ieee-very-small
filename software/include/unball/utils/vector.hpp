31fbe1af00de9befd2b8346be38eb1f3dc8a6d37
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2015-07-27 05:18:51 -0300

    Creating 2-dimensional vector class.

---
+++ software/include/unball/utils/vector.hpp
@@ -0,0 +1,107 @@
+/**
+ * @file   vector.hpp
+ * @author Matheus Vieira Portela
+ * @date   27/06/2015
+ *
+ * @attention Copyright (C) 2015 UnBall Robot Soccer Team
+ *
+ * @brief Two-dimensional vector class.
+ */
+
+#ifndef UNBALL_VECTOR_H_
+#define UNBALL_VECTOR_H_
+
+#include <cmath>
+#include <iostream>
+#include <string>
+
+#include <ros/ros.h>
+
+class Vector
+{
+  public:
+    // Empty constructor.
+    Vector();
+
+    // Initializes with pre-defined x and y values.
+    Vector(float x, float y);
+
+    // Copy constructor.
+    Vector(const Vector& other);
+
+    // Compound assignment operators.
+    Vector& operator+=(const Vector &rhs);
+    Vector& operator-=(const Vector &rhs);
+    Vector& operator*=(const Vector &rhs);
+    Vector& operator*=(float rhs);
+    Vector& operator/=(const Vector &rhs);
+
+    // Binary arithmetic operators.
+    const Vector operator+(const Vector &rhs) const;
+    const Vector operator-(const Vector &rhs) const;
+    const Vector operator*(const Vector &rhs) const;
+    const Vector operator*(float rhs) const;
+    const Vector operator/(const Vector &rhs) const;
+
+    // Comparison operators.
+    bool operator==(const Vector &rhs) const;
+    bool operator!=(const Vector &rhs) const;
+
+    // Getters and setters
+    float getX() const;
+    void setX(float x);
+
+    float getY() const;
+    void setY(float y);
+
+    // Calculates the magnitude, also known as norm or absolute value.
+    float getMagnitude() const;
+
+    // Calculates the direction (in radians).
+    float getDirection() const;
+
+    void set(float x, float y);
+    void set(Vector vector);
+
+    // Sets vector coordinates given magnitude and direction (in radians).
+    void setPolar(float magnitude, float direction);
+
+    // Adds two vectors in Cartesian coordinates.
+    void add(Vector vector);
+
+    // Subtracts two vectors in Cartesian coordinates.
+    void subtract(Vector vector);
+
+    // Multiplies vector coordinates by a scalar value.
+    void multiply(float scalar);
+
+    // Divides vector coordinates by a scalar value, except zero.
+    void divide(float scalar);
+
+    // Multiplies vector coordinates by -1.
+    void negate();
+
+    // Rotates vector by an angle (in radians) couterclockwise.
+    void rotate(float angle);
+
+    // Adjusts the x, y coordinates such that the vector norm is 1.
+    void normalize();
+
+    // Limits vector magnitude to the maximum given value.
+    void saturate(float maximum);
+
+    // Calculates the distance to another vector.
+    float calculateDistance(Vector vector) const;
+
+    // Converts to string for printing purposes.
+    std::string toString() const;
+
+  protected:
+    // 2-D (x, y) coordinates
+    float x_;
+    float y_;
+};
+
+std::ostream& operator<<(std::ostream& os, const Vector& vector);
+
+#endif  // UNBALL_VECTOR_H_
---
+++ software/src/unball/utils/vector.cpp
@@ -0,0 +1,233 @@
+/**
+ * @file   vector.cpp
+ * @author Matheus Vieira Portela
+ * @date   27/06/2015
+ *
+ * @attention Copyright (C) 2015 UnBall Robot Soccer Team
+ *
+ * @brief Two-dimensional vector class.
+ */
+
+#include <unball/utils/vector.hpp>
+
+Vector::Vector() : x_(0), y_(0) {}
+
+Vector::Vector(float x, float y) : x_(x), y_(y) {}
+
+Vector::Vector(const Vector& other)
+{
+    x_ = other.getX();
+    y_ = other.getY();
+}
+
+Vector& Vector::operator+=(const Vector &rhs)
+{
+    x_ += rhs.getX();
+    y_ += rhs.getY();
+    return *this;
+}
+
+Vector& Vector::operator-=(const Vector &rhs)
+{
+    x_ -= rhs.getX();
+    y_ -= rhs.getY();
+    return *this;
+}
+
+Vector& Vector::operator*=(const Vector &rhs)
+{
+    x_ *= rhs.getX();
+    y_ *= rhs.getY();
+    return *this;
+}
+
+Vector& Vector::operator*=(float rhs)
+{
+    x_ *= rhs;
+    y_ *= rhs;
+    return *this;
+}
+
+Vector& Vector::operator/=(const Vector &rhs)
+{
+    if (rhs.getX() == 0 || rhs.getY() == 0)
+    {
+        ROS_ERROR("[Vector] Division by vector with zero component");
+    }
+
+    x_ /= rhs.getX();
+    y_ /= rhs.getY();
+    return *this;
+}
+
+const Vector Vector::operator+(const Vector &rhs) const
+{
+    return Vector(*this) += rhs;
+}
+
+const Vector Vector::operator-(const Vector &rhs) const
+{
+    return Vector(*this) -= rhs;
+}
+
+const Vector Vector::operator*(const Vector &rhs) const
+{
+    return Vector(*this) *= rhs;
+}
+
+const Vector Vector::operator*(float rhs) const
+{
+    return Vector(*this) *= rhs;
+}
+
+const Vector Vector::operator/(const Vector &rhs) const
+{
+    return Vector(*this) /= rhs;
+}
+
+bool Vector::operator==(const Vector &rhs) const
+{
+    bool equalX = x_ == rhs.getX();
+    bool equalY = y_ == rhs.getY();
+    return (equalX && equalY);
+}
+
+bool Vector::operator!=(const Vector &rhs) const
+{
+    return !(*this == rhs);
+}
+
+float Vector::getX() const
+{
+    return x_;
+}
+
+void Vector::setX(float x)
+{
+    x_ = x;
+}
+
+float Vector::getY() const
+{
+    return y_;
+}
+
+void Vector::setY(float y)
+{
+    y_ = y;
+}
+
+float Vector::getMagnitude() const
+{
+    return hypot(x_, y_);
+}
+
+float Vector::getDirection() const
+{
+    return atan2(y_, x_);
+}
+
+void Vector::set(float x, float y)
+{
+    x_ = x;
+    y_ = y;
+}
+
+void Vector::set(Vector vector)
+{
+    x_ = vector.getX();
+    y_ = vector.getY();
+}
+
+void Vector::setPolar(float magnitude, float direction)
+{
+    x_ = magnitude*cos(direction);
+    y_ = magnitude*sin(direction);
+}
+
+void Vector::add(Vector vector)
+{
+    x_ += vector.getX();
+    y_ += vector.getY();
+}
+
+void Vector::subtract(Vector vector)
+{
+    x_ -= vector.getX();
+    y_ -= vector.getY();
+}
+
+void Vector::multiply(float scalar)
+{
+    x_ *= scalar;
+    y_ *= scalar;
+}
+
+void Vector::divide(float scalar)
+{
+    if (scalar == 0.0)
+    {
+        ROS_ERROR("[Vector] Division by scalar zero");
+    }
+
+    x_ /= scalar;
+    y_ /= scalar;
+}
+
+void Vector::negate()
+{
+    multiply(-1);
+}
+
+void Vector::rotate(float angle)
+{
+    float new_x = x_*cos(angle) - y_*sin(angle);
+    float new_y = x_*sin(angle) + y_*cos(angle);
+
+    x_ = new_x;
+    y_ = new_y;
+}
+
+void Vector::normalize()
+{
+    float magnitude = getMagnitude();
+
+    if (magnitude > 0)
+    {
+        x_ /= magnitude;
+        y_ /= magnitude;
+    }
+    else
+    {
+        ROS_WARN("[Vector] Normalizing a zero magnitude vector does not change anything");
+    }
+}
+
+void Vector::saturate(float maximum)
+{
+    if (getMagnitude() > maximum)
+    {
+        normalize();
+        multiply(maximum);
+    }
+}
+
+float Vector::calculateDistance(Vector vector) const
+{
+    return hypot(vector.getX() - x_, vector.getY() - y_);
+}
+
+std::string Vector::toString() const
+{
+    char buffer[64];
+    sprintf(buffer, "(%f, %f)", x_, y_);
+    std::string stringBuffer = buffer;
+
+    return stringBuffer;
+}
+
+std::ostream& operator<<(std::ostream& os, const Vector& vector)
+{
+    os << vector.toString();
+    return os;
+}
