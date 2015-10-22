a600f21fe2a2dcef735e99498bce1703cb6a16e1
imota <mota.icaro@gmail.com>
2015-10-22 21:08:58 -0200

    Goalkeeper functional and ready to go. Only have to reposition it to be a little closer to the goal, but I intend to do that creating another player that walks towards the right starting position of the goalkeeper.

--- software/src/unball/strategy/goalkeeper.cpp
+++ software/src/unball/strategy/goalkeeper.cpp
@@ -11,13 +11,13 @@ void Goalkeeper::buildPotentialFields(int robot_number)
     Vector robot_pos(robot[robot_number].getX(), robot[robot_number].getY());
     Vector ball_line(robot[robot_number].getX(), ball_pos_.getY());
 
-    //if (isBallBelowGoalkeeper(robot_number))
-    //{
+    if (isBallBelowGoalkeeper(robot_number))
+    {
     	if (isBallInRange())
 			potential_fields_.push_back(new AttractivePotentialField(ball_line, 20));
 		else 
 			stayAtTheBoundary();
-    //}
+    }
     //else
     //{
     	//Danger: block the goal like there is no tomorrow
@@ -32,11 +32,7 @@ void Goalkeeper::updateBallPos()
 
 bool Goalkeeper::isBallBelowGoalkeeper(int goalkeeper_number)
 {
-    Vector robot_pos(robot[goalkeeper_number].getX(), robot[goalkeeper_number].getY());
-    Vector difference = robot_pos - ball_pos_;
-    float angle = math::reduceAngle(difference.getDirection());
-
-	return math::quadrant(angle) == 3 or math::quadrant(angle) == 4;
+	return (ball_pos_.getX() < robot[2].getX());
 }
 
 bool Goalkeeper::isBallInRange()
