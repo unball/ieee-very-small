71584f847362c3dcaa4c35db3129a14acbad14fc
vitor duarte <vitor.alves.duarte@gmail.com>
2016-01-11 01:13:27 -0200

    remove old scripts

--- hardware/send_commands.py
+++
@@ -1,9 +0,0 @@
-import serial
-import struct
-
-s = serial.Serial('/dev/cu.usbserial-A600e0ti', baudrate=19200)
-
-while True:
-    data_to_write = struct.pack('=B', input('>> '))
-    print 'Data:', data_to_write
-    s.write(data_to_write)
