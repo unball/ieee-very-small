ab0556b0812d45ac6e33d22a62d867078af1efbd
gabrielnaves <gabri.navess@gmail.com>
2016-02-27 16:51:04 -0300

    Deleted the old vision implementation, as we're starting from scratch.

--- software/include/unball/vision/vision.hpp
+++ software/include/unball/vision/vision.hpp
@@ -1,8 +1,7 @@
 /**
  * @file   vision.hpp
- * @author Matheus Vieira Portela
  * @author Gabriel Naves da Silva
- * @date   25/03/2014
+ * @date   27/02/2016
  *
  * @attention Copyright (C) 2014 UnBall Robot Soccer Team
  *
@@ -20,39 +19,12 @@
 
 #include <opencv2/opencv.hpp>
 
-#include <unball/vision/gui.hpp>
-#include <unball/vision/preprocessor.hpp>
-#include <unball/vision/segmenter.hpp>
-#include <unball/vision/tracker.hpp>
-#include <unball/vision/homography.hpp>
-
 class Vision
 {
   public:
-    void setRGBFrame(cv::Mat rgb_frame);
-    void setDepthFrame(cv::Mat depth_frame);
-    bool isValidSize(cv::Mat frame);
-    std::vector<float> getRobotPose(int robot_index);
-    cv::Point2f getBallPose();
-    void loadConfig();
     void run();
 
-    static Vision& getInstance();
-
   private:
-    static Vision *instance; // Singleton instance
-
-    bool using_rgb_;
-    bool using_depth_;
-
-    GUI gui_;
-    Preprocessor preprocessor_;
-    Segmenter segmenter_;
-    Tracker tracker_;
-    Homography homography_;
-
-    cv::Mat rgb_frame_;
-    cv::Mat depth_frame_;
 };
 
 #endif // UNBALL_VISION_H_
