8d59f5454034a87bdc72de4897eae7131e90a493
Ícaro <mota.icaro@gmail.com>
2016-09-19 21:34:02 -0300

    Changed the radio_pipe file to be a generic communication protocol between the computer and the robots.

--- firmware/2016/radio_pipe/radio_pipe.ino
+++ firmware/2016/radio_pipe/radio_pipe.ino
@@ -1,165 +1,80 @@
 #include <SPI.h>
 #include "RF24.h"
 
-
-/****************** User Config ***************************/
-/***      Set this radio as radio number                ***/
-int radioNumber = 1; //Central=0
-
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 2 & 3 */
+/* Hardware configuration: Set up nRF24L01 radio on SPI protocol*/
 RF24 radio(3,2);    //arduino nano
-//RF24 radio(10,A0);
+//RF24 radio(10,A0); //arduino pro micro
 /**********************************************************/
 
-byte pipe[][6] = {"1Node","2Node","3Node","4Node"};
-
+byte pipe[][6] = {"1Node","2Node","3Node","4Node"}; 
+int receive_msg[1]; //pipe number
 
 void setup() {
   Serial.begin(250000);
   Serial.println("Inicio");
   radio.begin();
-
-  if(radio.isPVariant()){
-    Serial.println("versao nrf24l01+");
-  }
-  Serial.println("Insira qualquer caractere");
   
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_2MBPS);
   radio.setChannel(108);
 
-
-  /*
-  if(radioNumber){
-    radio.openWritingPipe(pipe[1]);
-    //radio.openReadingPipe(1,pipe[0]);
-  }else{
-    //
-    radio.openReadingPipe(1,pipe[1]);
-    // Start the radio listening for data
-    radio.startListening();
-  }
-  */
-  //settings for n0 for default
-  radio.openReadingPipe(1,pipe[1]);  //cetral->n0 pelo pipe1
-  radio.openWritingPipe(pipe[0]);    //n0->central pelo pipe0
-  
-  // Start the radio listening for data
+  radio.openReadingPipe(0,pipe[0]); //n1->central pelo pipe0  
   radio.startListening();
-
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
 }
-int msg[2]={0,0};
-int error=0;
-int received=0;
-int Nrobos=0;
-void loop() {
-  if(radioNumber==0){
-    //Central apenas recebe dados
-    if(radio.available()){
-      while (radio.available()) {         
-        radio.read(&msg, sizeof(msg));       
-      }
-      /*
-      for(int i=0; i<(sizeof(msg)/sizeof(int)); i++){
-        Serial.print(msg[i]);
-        Serial.print(" ");
-      }
-      Serial.println();
-      */
-      int command=0;
-      if(msg[0]==0){
-        Nrobos++;
-        command=1;//mudar de pipe
-        radio.stopListening();
-        radio.openWritingPipe(pipe[1]);
-        radio.write(&command, sizeof(command));
-        radio.startListening();
-      }
-    }
-  }else{
-    //Robos
-    if(radio.available()){
-      while(radio.available()){
-        radio.read(&received, sizeof(received));
-        if(received==53){
-          digitalWrite(7,HIGH);
-        }
-        if(received==54){
-          digitalWrite(7, LOW);
-        }
-        if(received==55){
-          radio.stopListening();
-          radio.openReadingPipe(1,pipe[1]);
-          msg[0] = 1;
-          radio.startListening();
-        }
-        if(received==56){
-          radio.stopListening();
-          radio.openReadingPipe(1,pipe[2]);
-          msg[0] = 2;
-          radio.startListening();
-        }
-        if(received==57){
-          radio.stopListening();
-          radio.openReadingPipe(1,pipe[3]);
-          msg[0] = 3;
-          radio.startListening();
-        }
-      }
-    }
-    msg[1] = error;
-    radio.stopListening();
-    digitalWrite(8, HIGH);
-    if(!(radio.write(&msg, sizeof(msg)))){
-      error++;
-    }
-    //error=error+radio.write(&msg, sizeof(msg),0);
-    radio.startListening();
-    digitalWrite(8, LOW);
+
+void checkIfRadioIsPVariant(){
+  if(radio.isPVariant()) {
+    Serial.println("versao nrf24l01+");
   }
-  delay(10);
 }
 
-void serialEvent() {
-  //input no terminal enviado aos receivers
-  //inteiro por inteiro
+bool receive_data_from_robot() {
+  if(radio.available()){
+    while (radio.available()) {         
+      radio.read(&receive_msg, sizeof(receive_msg));       
+    }
+    return true;
+  }
+  return false;
+}
+
+void send_data_to_ROS() {
+  Serial.println(receive_msg[0]); //Serial.write() may be a better choice for this
+  Serial.println(receive_msg[1]); //Serial.write() may be a better choice for this
+}
+
+String input_string;
+int string_length = 0;
+
+bool receive_data_from_ROS() {
   if(Serial.available()){
-    received=Serial.read();
-    Serial.print(received);
-    if(received==50||received==51){
-      if(received==50){
-        received=Serial.read();
-        Serial.print(received);
-        radio.stopListening();
-        radio.openWritingPipe(pipe[2]);
-        radio.write(&received, sizeof(received));
-        radio.startListening();
-      }else{
-        received=Serial.read();
-        Serial.print(received);
-        radio.stopListening();
-        radio.openWritingPipe(pipe[3]);
-        radio.write(&received, sizeof(received));
-        radio.startListening();
-      }
-    }else{
-      radio.stopListening();
-      radio.openWritingPipe(pipe[1]);
-      radio.write(&received, sizeof(received));
-      radio.startListening();
+    input_string = "";
+    while (Serial.available()) {         
+      char in_char = (char)Serial.read(); //taken from the SerialEvent example
+      input_string += in_char;
     }
-    Serial.println();
+    if (input_string.length() == string_length)
+      return true;
   }
-  if(radioNumber==1){
-    Serial.println("Central started");
-    radioNumber=0;
-    radio.stopListening();
-    radio.openWritingPipe(pipe[1]);  //central->n1 pelo pipe1
-    radio.openReadingPipe(0,pipe[0]);//n1->central pelo pipe0
-    // Start the radio listening for data
-    radio.startListening();
+  return false;
+}
+
+void send_data_to_robot() {
+  if(radio.available()){
+    while (radio.available()) {         
+      radio.read(&receive_msg, sizeof(receive_msg));       
+    }
+    return true;
   }
+  return false;  
+}
+
+void loop() {
+  if (receive_data_from_robot())
+    send_data_to_ROS();
+  if (receive_data_from_ROS())
+    send_data_to_robot();
+  
+  delay(10);
 }
 
