eeb13948bb06f76f7b988c3a967e390a4940b1ee
Ícaro <mota.icaro@gmail.com>
2016-10-03 18:15:32 -0300

    Started refactor of the radio segment of the firmware.

--- firmware/2016/robot_firmware/robot_firmware.ino
+++ firmware/2016/robot_firmware/robot_firmware.ino
@@ -1,24 +1,30 @@
 #include <SPI.h>
 #include "RF24.h"
 
+int LED = 6;
+
+/*Motor Variables*/
+int PWMA = 9;
+int AIN1 = 8;
+int AIN2 = 7;
+int PWMB = 5;
+int BIN1 = 4;
+int BIN2 = A3;
+
+/*Radio Variables*/
 /*Radio pins depend on the Arduino used*/
 RF24 radio(10,A0);    //arduino pro micro
 //RF24 radio(3, 2);   //arduino nano
-
 /*The starting LISTENING pipe defines the "lost robots" zone. The communication between
 the central and the robots will not be made through this pipe. This pipe will 
 only be used to tell the robot which pipe it should switch to*/
 int starting_pipe = 0;
-
 /*Actual pipe the robot will use to LISTEN. The writting pipe number is, by default, 
  * the listening pipe number + 1 (my_pipe+1)*/
 int my_pipe;
-
 byte pipe[][6] = {"1Node","2Node","3Node","4Node"};
-
 #define SEND_MESSAGE_SIZE 2
 int send_msg[SEND_MESSAGE_SIZE];  //The message the robot will send to the central
-
 #define RECEIVED_MESSAGE_SIZE 2
 int msg_from_central[RECEIVED_MESSAGE_SIZE] = {0,0};
 
@@ -33,17 +39,16 @@ void setPipes(int new_pipe) {
 }
 
 void radioSetup() {
+  radio.begin();
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_2MBPS);
   radio.setChannel(108);
+  setPipes(starting_pipe);
+  radio.startListening();
 }
 
 void setup() {
-  radio.begin();
   radioSetup();
-  
-  setPipes(starting_pipe);
-  radio.startListening();
 }
 
 void send() {
