18dcae0b5f602aa8b9333d6188a0587b9b249f71
Ícaro <mota.icaro@gmail.com>
2016-10-07 08:09:34 -0300

    Started a new program to test a new board, as Naves insistently asked me. Radio and motor files are exactly the same used in the actual firmware.

---
+++ firmware/2016/test_new_board/test_new_board.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+#include "RF24.h"
+
+#define MOTOR_A "motorA"
+#define MOTOR_B "motorB"
+
+int LED = 6;
+bool has_received_message;
+
+void setup() {
+  radioSetup();
+  motorsSetup();
+}
+
+void sendMessageBackToCentral() {
+    int send_message[2];
+    getMessages(send_message);
+    send(send_message);   
+}
+
+void setSpeeds() {
+  move(100, MOTOR_A);
+  move(100, MOTOR_B);
+}
+
+void loop() {
+  has_received_message = receive();
+
+  setSpeeds();
+  if (has_received_message){
+    if (isStartingPipe())
+      setChannel();
+  }
+  
+  delay(20);
+  has_received_message = false;
+}
