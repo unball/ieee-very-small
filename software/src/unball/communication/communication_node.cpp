5c246d3f325e6ad0616242ecb62edac611062599
imota <mota.icaro@gmail.com>
2016-10-09 04:16:12 -0300

    I think that now the communication node compiles

--- software/src/unball/communication/communication_node.cpp
+++ software/src/unball/communication/communication_node.cpp
@@ -22,6 +22,7 @@
 void receiveStrategyMessage(const unball::StrategyMessage::ConstPtr &msg);
 float calculateLeftSpeed(int i);
 float calculateRightSpeed(int i);
+float convertSpeedToRpm(float speed);
 
 float lin_vel[3];
 float ang_vel[3];
@@ -33,7 +34,7 @@ int main(int argc, char **argv)
 {
 	boost::asio::io_service ios;
 	boost::asio::serial_port sp(ios, "/dev/ttyUSB0");
-	sp.set_option(boost::asio::serial_port::baud_rate(250000));
+	sp.set_option(boost::asio::serial_port::baud_rate(19200));
 	
 	ros::init(argc, argv, "communication_node");
     ros::NodeHandle n;
@@ -44,21 +45,20 @@ int main(int argc, char **argv)
     while (ros::ok())
     {
 		char robot_number, right_wheel, left_wheel;
-		char robot_number_char, right_wheel_char, left_wheel_char;
 		std::string message;
 
 		for (int i = 0; i < 3; i++)
 		{
-			robot_number = i + 48;
-			right_wheel = (int)calculateRightSpeed(i) + 48;
-			left_wheel = (int)calculateLeftSpeed(i) + 48;
+			robot_number = i;
+			right_wheel = (int)calculateRightSpeed(i);
+			left_wheel = (int)calculateLeftSpeed(i);
 
 			message.push_back(robot_number); message.push_back(right_wheel); message.push_back(left_wheel);
 
-			ROS_ERROR("Robot: %c\tLeft: %c\tRight: %c", i, left_wheel, right_wheel);
+			ROS_ERROR("Robot: %d\tLeft: %d\tRight: %d", i, left_wheel, right_wheel);
 
 			sp.write_some(boost::asio::buffer(message, message.size()));
-		}	
+		}
         
         ros::spinOnce();
         loop_rate.sleep();
@@ -81,7 +81,7 @@ void receiveStrategyMessage(const unball::StrategyMessage::ConstPtr &msg)
 
 float calculateLeftSpeed(int i)
 {
-	return 100;
+	return 2;
 	float linear_speed_rpm = convertSpeedToRpm(lin_vel[i]);
 	float tangential_speed = ang_vel[i]*(WHEELS_DISTANCE/2);
 	float tangential_speed_rpm = convertSpeedToRpm(tangential_speed);
@@ -90,7 +90,7 @@ float calculateLeftSpeed(int i)
 
 float calculateRightSpeed(int i)
 {
-	return 70;
+	return 100;
 	float linear_speed_rpm = convertSpeedToRpm(lin_vel[i]);
 	float tangential_speed = ang_vel[i]*(WHEELS_DISTANCE/2);
 	float tangential_speed_rpm = convertSpeedToRpm(tangential_speed);
