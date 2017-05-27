6431897c45087ba2e0e7166e10c2c5a3d422261f
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-05-27 16:28:02 -0300

    Measurement system no longer has conversion code in it

--- software/src/unball/measurement_system/measurement_system_node.cpp
+++ software/src/unball/measurement_system/measurement_system_node.cpp
@@ -1,39 +1,12 @@
-/**
- * @file   measurement_systen_node.cpp
- * @author Izabella Thais Oliveira Gomes
- * @date   23/09/2016
- *
- * @attention Copyright (C) 2014 UnBall Robot Soccer Team
- *
- * @brief Rotate robot position datas acording to camera position in the vision and simulator and publishes that.
- *
-*/
-
 #include <vector>
 #include <cmath>
 
 #include <ros/ros.h>
 
-#include <unball/VisionMessage.h>
-#include <unball/SimulatorMessage.h>
+#include <vision/PixelToMetricConversionMessage.h>
 #include <unball/MeasurementSystemMessage.h>
-#include <opencv2/opencv.hpp>
 
-//const float field_x_length = 1.50;
-//const float field_y_length = 1.30;
-
-const float field_x_length = 1.60;
-const float field_y_length = 1.30;
-
-
-const float camera_x_length = 640;
-const float camera_y_length = 480;
-
-void receiveVisionMessage(const unball::VisionMessage::ConstPtr &msg_s);
-void rotateAxis();
-void convertPixelsToMeters();
-void receiveSimulatorMessage(const unball::SimulatorMessage::ConstPtr &msg_v);
-void publishMeasurementSystemMessage(ros::Publisher &publisher);
+void receivePxToMMessage(const vision::PixelToMetricConversionMessage::ConstPtr &msg_s);
 void filter();
 
 unball::MeasurementSystemMessage message;
@@ -47,7 +20,7 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     ros::Rate loop_rate(10); // Hz
 
-    ros::Subscriber sub = n.subscribe("vision_topic", 1, receiveVisionMessage);
+    ros::Subscriber sub = n.subscribe("pixel_to_metric_conversion_topic", 1, receivePxToMMessage);
     publisher = n.advertise<unball::MeasurementSystemMessage>("measurement_system_topic", 1);
 
     while (ros::ok())
@@ -60,40 +33,28 @@ int main(int argc, char **argv)
 }
 
 bool shouldConvert[3] = { true, true, true};
-void receiveVisionMessage(const unball::VisionMessage::ConstPtr &msg_v)
+void receivePxToMMessage(const vision::PixelToMetricConversionMessage::ConstPtr &msg_v)
 {
     std::vector<float> x(6), y(6), th(6);
     std::vector<float> ball_location(2);
 
-    ROS_INFO("\n\n[MeasurementNode]:ReceiveVisionMessage - Receiving vision message");
+    ROS_INFO("\n\n[MeasurementNode]:receivePxToMMessage - Receiving vision message");
 
     for (int robot_index = 0; robot_index < 6; robot_index++)
     {
         ROS_INFO("%d x: %f\t y: %f\t th: %f", robot_index, msg_v->x[robot_index], msg_v->y[robot_index],
             msg_v->th[robot_index]);
-        //if (not (msg_v->x[robot_index] == -1 and msg_v->y[robot_index] == -1)) 
-        
-        if(msg_v->y[robot_index] > 1 and msg_v->y[robot_index] < 480)
-        { 
-            message.x[robot_index] = msg_v->x[robot_index];
-            message.y[robot_index] = msg_v->y[robot_index];
-            if (not std::isnan(msg_v->th[robot_index])){
-                message.th[robot_index] = msg_v->th[robot_index];
-            }
-            shouldConvert[robot_index] = true;
 
-        }
-        else
-            shouldConvert[robot_index] = false;        
-
+        message.x[robot_index] = msg_v->x[robot_index];
+        message.y[robot_index] = msg_v->y[robot_index];
+        message.th[robot_index] = msg_v->th[robot_index];
     }
     message.ball_x = msg_v->ball_x;
     message.ball_y = msg_v->ball_y;
-    convertPixelsToMeters();
 
     //filter();
 
-    ROS_INFO("\n\n[MeasurementNode]:ReceiveVisionMessage - Sending measurement system message");
+    ROS_INFO("\n\n[MeasurementNode]:receivePxToMMessage - Sending measurement system message");
 
     for (int robot_index = 0; robot_index < 6; robot_index++)
     {
@@ -103,35 +64,6 @@ void receiveVisionMessage(const unball::VisionMessage::ConstPtr &msg_v)
     ROS_INFO("Ball: x: %f, y: %f", message.ball_x, message.ball_y);
 
     publisher.publish(message);
-}
-
-void convertPixelsToMeters(){
-    auto x_conversion = field_x_length / camera_x_length;
-    auto y_conversion = (field_y_length / camera_y_length) * -1;
-    for (int i = 0; i < 6; ++i)
-    {
-        if (i<3) 
-        {
-            if (shouldConvert[i]) 
-            {
-                message.x[i] -= camera_x_length / 2;
-                message.y[i] -= camera_y_length / 2;
-                message.x[i] *= x_conversion;
-                message.y[i] *= y_conversion;
-            }
-        }
-        else 
-        {
-            message.x[i] -= camera_x_length / 2;
-            message.y[i] -= camera_y_length / 2;
-            message.x[i] *= x_conversion;
-            message.y[i] *= y_conversion;
-        }
-    }
-    message.ball_x -= camera_x_length / 2;
-    message.ball_y -= camera_y_length / 2;
-    message.ball_x *= x_conversion;
-    message.ball_y *= y_conversion;
 }
 
 float mean_array_x[6]={0,0,0,0,0,0};
@@ -150,7 +82,7 @@ void filter(){
 
         mean_array_y[i] += (message.y[i] - mean_array_y[i])/N_mean;
         message.y[i] = mean_array_y[i];
-        
+
         /*
         if (not std::isnan(mean_array_th[i] + (message.th[i] - mean_array_th[i])/N_mean)) {
             if(abs(message.th[i]-mean_array_th[i])>M_PI/2){
@@ -162,7 +94,7 @@ void filter(){
         }
         */
     }
-    
+
     mean_ball_x += (message.ball_x - mean_ball_x)/N_mean;
     message.ball_x = mean_ball_x;
 
