3496571d2bb31f69dc43515eefbe91c07cec6ccf
imota <mota.icaro@gmail.com>
2017-06-10 15:09:57 -0300

    Took the control part as it was from the robot_firmware to a new file

---
+++ firmware/headers/control.h
@@ -0,0 +1,79 @@
+void stand(){
+  int acc=0;
+  while(true){
+    if(radio.available()){
+       acc=0; 
+       int temp1=velocidades.motorA;
+       int temp2=velocidades.motorB;
+       while(radio.available()){       
+        radio.read(&velocidades,sizeof(velocidades));
+       }
+       if(!(velocidades.motorA==temp1 && velocidades.motorB==temp2)){
+        //errorA_i=0;
+        //errorB_i=0;  
+       }
+     }else{
+      //procedimento para indicar que o robo nao recebe mensagens nas ultimas 20000 iteracoes
+      acc++;
+      if(acc>20000){
+        control(200,-200);
+      }else{
+        control(velocidades.motorA, velocidades.motorB);
+      }
+    }
+  }
+}
+
+void control(int velocidadeA, int velocidadeB){
+  if(velocidadeA || velocidadeB){
+    encoder();
+    Serial.print("  motor0: ");Serial.print(contadorA);Serial.print("//");Serial.print(contadorA_media);
+    Serial.print("  motor1: ");Serial.print(contadorB);Serial.print("//");Serial.print(contadorB_media);
+    long errorA=velocidadeA-contadorA_media;
+    long errorB=velocidadeB-contadorB_media;
+    errorA_i+=errorA;
+    errorB_i+=errorB;
+    
+    Serial.print(" error:");
+    Serial.print(errorA);
+    Serial.print("||");
+    Serial.print(errorB);
+    
+    long ke_a=1200;
+    long ki_a=30;
+    
+    long ke_b=1200;
+    long ki_b=30;
+
+    long intermediarioA=0;
+    intermediarioA=(ke_a*errorA)/1000;
+    intermediarioA+=(ki_a*errorA_i)/1000;
+
+    long intermediarioB=0;
+    intermediarioB=(ke_b*errorB)/1000;
+    intermediarioB+=(ki_b*errorB_i)/1000;
+
+    int commandA=intermediarioA;
+    int commandB=intermediarioB;
+    commandA_media+=(commandA-commandA_media)/10;
+    commandB_media+=(commandB-commandB_media)/10;
+
+    if(commandA_media>255){
+      commandA_media=255;
+    }
+    if(commandB_media>255){
+      commandB_media=255;
+    }
+    
+    move(0, commandA_media);
+    move(1, commandB_media);
+    Serial.print("   commands ");
+    Serial.print(commandA);Serial.print("//");Serial.print(commandA_media);
+    Serial.print(" ");Serial.print(commandB);Serial.print("//");Serial.println(commandB_media);
+  }else{
+    contadorA_media=0;
+    contadorB_media=0;
+    move(0,0);
+    move(1,0);
+  }
+}
