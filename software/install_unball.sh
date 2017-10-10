3570f70b87771567a508c6cde6adae749c3b6fbe
Vieira Neto <vieiranetoc@gmail.com>
2017-10-10 19:20:29 -0300

    fixed ros packages installations

--- software/install_unball.sh
+++ software/install_unball.sh
@@ -118,14 +118,7 @@ rosversion="ros-kinetic"
 
 ros_tools=(
     $rosversion"-joy"
-    $rosversion"-bridge-server"
-    $rosversion"-rosserial"
-    $rosversion"-rosserial-arduino"
-)
-
-ros_tools=(
-    $rosversion"-joy"
-    $rosversion"-bridge-server"
+    $rosversion"-rosbridge-server"
     $rosversion"-rosserial"
     $rosversion"-rosserial-arduino"
 )
@@ -137,7 +130,6 @@ install_dependency "Developer tools and packages" devtools[@]
 install_dependency "GTK development library" gtk[@]
 install_dependency "Video I/O packages" video_iopack[@]
 install_dependency "Python 2.7 dev tools" python_dev[@]
-install_dependency "ROS Dependencies" ros_tools[@]
 
 if ! [[ ! -x "$(command -v roscore)" ]];then
   echo $(echo_pass 'ros')
@@ -146,5 +138,6 @@ else
   configure_catkin
 fi
 
+install_dependency "ROS Dependencies" ros_tools[@]
 
-configld
+# configld
