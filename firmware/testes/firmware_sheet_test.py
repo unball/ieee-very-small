a66ab91f2961c9bc66e7c3afb18354c871b451ba
Anderson Barros Rodrigues <makiander.abr@gmail.com>
2017-10-25 18:20:58 -0200

    Upgrated the motor test, now the test now plots a graph in html

--- firmware/testes/firmware_sheet_test.py
+++ firmware/testes/firmware_sheet_test.py
@@ -1,5 +1,7 @@
 import xlsxwriter
 import serial
+import plotly
+import plotly.graph_objs as go
 
 
 #for x in range(1,3):
@@ -11,6 +13,7 @@ import serial
 #	eb = input("erro B: ")
 #	worksheet.write(x, 2, eb)
 def ler_serial():
+	array = [[i*0 for i in range(3000)],[i*0 for i in range(3000)],[i*0 for i in range(3000)]]
 	start = False
 	x = 1
 	y = 0
@@ -19,19 +22,27 @@ def ler_serial():
 		if data == "$":
 			y=0
 			data = arduino.readline()[:-2]
+			array[y][x-1] = data
 			print data
 			worksheet.write(x, y, data)
 			y+=1
 			data = arduino.readline()[:-2]
+			array[y][x-1] = data
 			print data
 			worksheet.write(x, y, data)
 			y+=1
 			data = arduino.readline()[:-2]
+			array[y][x-1] = data
 			print data
 			worksheet.write(x, y, data)
 			x+=1
 		elif data == "#":
-			print "TERMINA AQUI"
+			print "TERMINOU AQUI"
+			trace0 = go.Scatter(y=array[0], name='input')
+			trace1 = go.Scatter(y=array[1], name='Motor A')
+			trace2 = go.Scatter(y=array[2], name='Motor B')
+			graph = [trace0, trace1, trace2]
+			plotly.offline.plot(graph, filename='my-graph.html')
 			break;
 		
 	
@@ -43,7 +54,7 @@ worksheet = workbook.add_worksheet()
 arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=.1)
 
 #worksheet.write(2, 0, 123)
-worksheet.write('A1', 'tensao')
+worksheet.write('A1', 'input')
 worksheet.write('B1', 'motor A')
 worksheet.write('C1', 'motor B')
 ler_serial()
